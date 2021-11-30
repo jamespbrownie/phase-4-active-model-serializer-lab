51d3cd8e60cbbca80bdc4d52ccbe1974fb2cc289
James Brownie <jamespbrownie@gmail.com>
2021-11-30 15:07:00 -0600

    commit tree 100ed4eca1a9f9174d570fff62e201489bf64621

---
+++ app/serializers/author_post_serializer.rb
@@ -0,0 +1,13 @@
+class AuthorPostSerializer < ActiveModel::Serializer
+  attributes :title, :short_content, :tags
+
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
+
+  belongs_to :author
+  has_many :tags
+  # , through: :post_tags
+
+end
